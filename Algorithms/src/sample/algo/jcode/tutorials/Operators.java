9e976188659e225f3b42716d07cd53294cb5398e
Rishu Shrivastava <rishu.shrivastava@gmail.com>
2015-12-16 19:38:01 +0530

    Operator sample code

---
+++ Algorithms/src/sample/algo/jcode/tutorials/Operators.java
@@ -0,0 +1,24 @@
+package sample.algo.jcode.tutorials;
+
+/**
+ * Working on the Operators in Java. Basic sample codes.
+ * @author Rishu
+ *
+ */
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int A=10;
+		int B=20;
+		
+		System.out.println("A= "+A);
+		System.out.println("B= "+B);
+		System.out.println("Assignment Operator = :"+ (A==B));
+		System.out.println("Addition Operator :"+ (A+B));
+		System.out.println("Bitwise XOR Operator :"+ (A^B));
+
+	}
+
+}
